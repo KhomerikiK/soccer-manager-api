9abc86a56610fffacf57e7345e38b5a5c286887b
konstantinekhomeriki <khomeriki.dev@gmail.com>
2023-05-21 04:08:30 +0400

    feat: Store initial balance on Registered event

---
+++ app/Listeners/SetInitialBalanceToRegisteredUser.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Enums\TransactionType;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\DB;
+
+class SetInitialBalanceToRegisteredUser
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Registered $event): void
+    {
+        DB::transaction(function () use ($event) {
+            $initialAmount = toBaseUnit(5000000);
+            $event->user->balance = $initialAmount;
+            $event->user->save();
+            $event->user->transactions()->create([
+                'type' => TransactionType::CREDIT,
+                'amount' => $initialAmount,
+                'description' => 'Initial balance',
+            ]);
+        });
+    }
+}
